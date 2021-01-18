4790bd01131b69cf2a71e06e9528c70844a2a2f3
Pratik K <pratik.k98@yahoo.com>
2021-01-18 17:45:23 +0400

    [Update] refactor structure
    - code cleanup
    - parameterization
    - simple reporting

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# fin_playground
+# BTC Futures Strategy
+
+## Installation
+
+```bash
+pip3 install -r requirements.txt
+```
+
+## Running
+
+```bash
+usage: backtest.py [-h] [-i INPUT] [-o OUTPUT] [-s EMA_SLOW] [-f EMA_FAST]
+                   [-a ADX]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -i INPUT, --input INPUT
+                        OHLCV Data
+  -o OUTPUT, --output OUTPUT
+                        Order information data
+  -s EMA_SLOW, --ema_slow EMA_SLOW
+                        EMA Slow
+  -f EMA_FAST, --ema_fast EMA_FAST
+                        EMA Fast
+  -a ADX, --adx ADX     ADX period
+```
+
+## Development
+
+```bash
+pip3 install pip-tools
+pip-compile requirements.in > requirements.txt
+```
+
+
--- backtest.py
+++ backtest.py
@@ -1,83 +1,118 @@
+#!/usr/bin/env python3
+# coding: utf-8
+"""
+    :author: mystertech
+    :brief: BTC futures strategy
+"""
+import argparse
 from datetime import time
+
 import pandas as pd
-import matplotlib.pyplot as plt
 import talib
-import mplfinance as mpf 
-
-df=pd.read_csv("binance_data.csv")
-
-df['emaShort'] = talib.EMA(df["Close"], timeperiod=10)
-df['emaLong'] = talib.EMA(df["Close"], timeperiod=25)
-df['adx'] = talib.ADX(df['High'], df['Low'], df['Close'], timeperiod=14)
-signal = "None"
-inPosition = False
-sl=0
-tp=0
-buyPrice=0
-win=0
-loss=0
-ordersDF = pd.DataFrame(columns = ['Timestamp', 'BuyPrice', 'TP', 'SL', 'W/L'])
-orderTime = df.at[0,'Timestamp']
 
 
-for i in df.index:
-    if (signal=="None" and not(inPosition)):
-        if ((df.at[i,'emaShort']>df.at[i,'emaLong']) and (df.at[i-1,'emaShort']<df.at[i-1,'emaLong'])) :
-            if df.at[i,'adx']<40 and df.at[i,'adx']>30:
+def collect_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input", type=str, default="binance_data.csv",
+                        help="OHLCV Data")
+    parser.add_argument("-o", "--output", type=str, default="orders.csv",
+                        help="Order information data")
+
+    parser.add_argument("--sl", type=int, default=100,
+                        help="Stoploss")
+    parser.add_argument("--rr", type=float, default=1.5,
+                        help="Risk reward ratio 1:X")
+
+    parser.add_argument("-s", "--ema_slow", type=int,
+                        default=25, help="EMA Slow")
+    parser.add_argument("-f", "--ema_fast", type=int,
+                        default=10, help="EMA Fast")
+    parser.add_argument("-a", "--adx", type=int,
+                        default=14, help="ADX period")
+
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = collect_args()
+    df = pd.read_csv(args.input, parse_dates=[0])
+
+    df['emaFast'] = talib.EMA(df.Close, timeperiod=args.ema_fast)
+    df['emaSlow'] = talib.EMA(df.Close, timeperiod=args.ema_slow)
+    df['adx'] = talib.ADX(df.High, df.Low,
+                          df.Close, timeperiod=args.adx)
+
+    signal, status, inPosition, exited = None, None, False, False
+    sl, tp, buyPrice, win, loss = 0, 0, 0, 0, 0
+    orders = []
+    orderTime = df.iloc[0].Timestamp
+    n_candles, n_fields = df.shape
+
+    for i in range(n_candles):
+        print(
+            f"Processing | W:{win}|L:{loss} [{i + 1}/{n_candles}]", end="\r", flush=True)
+
+        # check entry signal condition
+        if signal is None and not inPosition:
+            if df.at[i, 'emaFast'] > df.at[i, 'emaSlow'] and \
+                    df.at[i - 1, 'emaFast'] < df.at[i - 1, 'emaSlow'] and \
+                    df.at[i, 'adx'] < 40 and df.at[i, 'adx'] > 30:
                 signal = "Short"
-        elif ((df.at[i,'emaShort']<df.at[i,'emaLong']) and (df.at[i-1,'emaShort']>df.at[i-1,'emaLong'])) :
-            if df.at[i,'adx']<40 and df.at[i,'adx']>30:
+            elif df.at[i, 'emaFast'] < df.at[i, 'emaSlow'] and \
+                    df.at[i - 1, 'emaFast'] > df.at[i - 1, 'emaSlow'] and \
+                    df.at[i, 'adx'] < 40 and df.at[i, 'adx'] > 30:
                 signal = "Long"
-    elif not(inPosition) :
-        buyPrice = (df.at[i,'Close'] + df.at[i,'Open'])/2
-        inPosition=True
-        orderTime = df.at[i,'Timestamp']
-        if signal=="Long" :
-            tp = buyPrice + 150
-            sl = buyPrice - 100
-        elif signal=="Short" :
-            tp = buyPrice - 150
-            sl = buyPrice + 100
-    else :
-        if signal=="Long" :
-            if tp<=df.at[i,'High'] :
-                win=win+1
-                ordersDF = ordersDF.append({'Timestamp': orderTime, 'BuyPrice': buyPrice, 'TP':tp, 'SL':sl, 'W/L': 'W'}, ignore_index = True) 
-                signal = "None"
-                inPosition = False
-                sl=0
-                tp=0
-                buyPrice=0
-            elif sl>=df.at[i,'Low'] :
-                loss=loss+1
-                ordersDF = ordersDF.append({'Timestamp': orderTime, 'BuyPrice': buyPrice, 'TP':tp, 'SL':sl, 'W/L': 'L'}, ignore_index = True)
-                signal = "None"
-                inPosition = False
-                sl=0
-                tp=0
-                buyPrice=0
-        elif signal=="Short" :
-            if tp>=df.at[i,'Low'] :
-                win=win + 1
-                ordersDF = ordersDF.append({'Timestamp': orderTime, 'BuyPrice': buyPrice, 'TP':tp, 'SL':sl, 'W/L': 'W'}, ignore_index = True)
-                signal = "None"
-                inPosition = False
-                sl=0
-                tp=0
-                buyPrice=0
-            elif sl<=df.at[i,'High'] :
-                loss=loss+1
-                ordersDF = ordersDF.append({'Timestamp': orderTime, 'BuyPrice': buyPrice, 'TP':tp, 'SL':sl, 'W/L': 'L'}, ignore_index = True)
-                signal = "None"
-                inPosition = False
-                sl=0
-                tp=0
-                buyPrice=0
 
-total = win+loss
-print ("total bets: " + str(total))
-print("win: "+ str(win) + " loss: "+ str(loss))
-winP = (win/total)*100
-print ("win% : " + str(winP))
-print (ordersDF.head)
-ordersDF.to_csv('orders.csv')
+        # enter if signal
+        elif not inPosition:
+            buyPrice = (df.at[i, 'Close'] + df.at[i, 'Open']) / 2
+            inPosition = True
+            orderTime = df.at[i, 'Timestamp']
+            if signal == "Long":
+                tp = buyPrice + (args.sl * args.rr)
+                sl = buyPrice - args.sl
+            elif signal == "Short":
+                tp = buyPrice - (args.sl * args.rr)
+                sl = buyPrice + args.sl
+
+        # wait for exit condition
+        else:
+            if signal == "Long":
+                if tp <= df.at[i, 'High']:
+                    status = 'W'
+                    exited = True
+                elif sl >= df.at[i, 'Low']:
+                    status = 'L'
+                    exited = True
+            elif signal == "Short":
+                if tp >= df.at[i, 'Low']:
+                    status = 'W'
+                    exited = True
+                elif sl <= df.at[i, 'High']:
+                    status = 'L'
+                    exited = True
+
+            # reset variables for next trade if exited
+            if exited:
+                win += status == 'W'
+                loss += status == 'L'
+                orders.append({
+                    'Timestamp': orderTime,
+                    'BuyPrice': buyPrice,
+                    'TP': tp,
+                    'SL': sl,
+                    'W/L': status
+                })
+                signal, status, inPosition, exited = None, None, False, False
+                sl, tp, buyPrice = 0, 0, 0
+
+    total = win + loss
+    print(
+        f"\nT: {total} :: W: {win} | L: {loss} [{round(win / total * 100, 2)}%]")
+    orders = pd.DataFrame(orders)
+    orders.to_csv(args.output, float_format="%.3f")
+
+
+if __name__ == "__main__":
+    main()
