c5cf9d1181b8dcf4e627f545696d4e2237a6dee0
MysterTech <cmsanjaykrishna@gmail.com>
2021-02-01 20:53:11 +0530

    [Bugfix]1.final working version

--- strategy.py
+++ strategy.py
@@ -10,8 +10,10 @@ import json
 import os
 import random
 import pandas
+import numpy as np
 
-import talib as ta
+import talib
+from talib import stream
 
 from utils.strategy_helpers import Strategy, create_streamer
 
@@ -29,25 +31,31 @@ RABBIT_URI = os.getenv("RABBIT_URI", "amqp://guest:guest@localhost/")
 on_stream = create_streamer(loop, RABBIT_URI)
 alpha = Strategy("alpha", loop)
 alpha.overlap, alpha.sigGenerated, alpha.signal = False, False, Signal.NULL
-alpha.eMAFast, alpha.eMASlow, alpha.aDXInterval = 10, 25, 14
+alpha.eMAFast, alpha.eMASlow, alpha.aDXInterval, alpha.orderPrice, alpha.sl, alpha.tp, secLatestEMAFast, secLatestEMASlow = 3, 6, 7, 0, 0, 0, 0, 0
+
+# Checks entry after signal is generated
 
 
 def checkEntry(data: dict):
     if (alpha.signal == Signal.LONG) and (data['m'] < data['b']):
-        alpha.orderprice = data['m']
-        alpha.sl = alpha.orderprice-100
-        alpha.tp = alpha.orderprice+150
-        print("entered long at " + str(data['t']) + " with mark price : " +
-              str(data['m']) + " exit at sl = " + str(alpha.sl) + " or tp = " + str(alpha.tp))
+        alpha.orderPrice = data['m']
+        alpha.sl = alpha.orderPrice-100
+        alpha.tp = alpha.orderPrice+150
+        print("entered long at " + str(data['t']) + " with entry price : " +
+              str(alpha.orderPrice) + " exit at sl = " + str(alpha.sl) + " or tp = " + str(alpha.tp))
+        alpha.signal = Signal.NULL
         return True
     elif (alpha.signal == Signal.SHORT) and (data['m'] > data['a']):
-        alpha.orderprice = data['m']
-        alpha.sl = alpha.orderprice+100
-        alpha.tp = alpha.orderprice-150
-        print("entered long at " + str(data['t']) + " with mark price : " +
-              str(data['m']) + " exit at sl = " + str(alpha.sl) + " or tp = " + str(alpha.tp))
+        alpha.orderPrice = data['m']
+        alpha.sl = alpha.orderPrice+100
+        alpha.tp = alpha.orderPrice-150
+        print("entered short at " + str(data['t']) + " with entry price : " +
+              str(alpha.orderPrice) + " exit at sl = " + str(alpha.sl) + " or tp = " + str(alpha.tp))
+        alpha.signal = Signal.NULL
         return True
     return False
+
+# Checks exit
 
 
 def checkExit(data: dict):
@@ -56,8 +64,10 @@ def checkExit(data: dict):
         print("exited LONG at " + str(data['t']) +
               " with mark price : " + str(data['m']))
         if (alpha.sl >= data['m']):
+            print("exit at sl")
             print("lose")
         elif (alpha.tp <= data['m']):
+            print("exit at tp")
             print("win")
         return True
     elif (alpha.sl > alpha.orderPrice) and ((alpha.sl <= data['m']) or (alpha.tp >= data['m'])):
@@ -65,29 +75,35 @@ def checkExit(data: dict):
         print("exited SHORT at " + str(data['t']) +
               " with mark price : " + str(data['m']))
         if (alpha.sl <= data['m']):
+            print("exit at sl")
             print("lose")
         elif (alpha.tp >= data['m']):
+            print("exit at tp")
             print("win")
         return True
     return False
 
+# Generates trade signals
+
 
 def genSig(data: dict):
-    global df
+
+    global df, secLatestEMAFast, secLatestEMASlow
     df = df.append(data, ignore_index=True)
 
-    # This potetially can be changed for performance
-    df['emaFast'] = ta.EMA(df.c, timeperiod=alpha.eMAFast)
-    df['emaSlow'] = ta.EMA(df.c, timeperiod=alpha.eMASlow)
-    df['adx'] = ta.ADX(df.h, df.l,
-                       df.c, timeperiod=alpha.aDXInterval)
+    latestEMAFast = stream.EMA(df['c'], timeperiod=alpha.eMAFast)
+    latestEMASlow = stream.EMA(df['c'], timeperiod=alpha.eMASlow)
+    latestADX = stream.ADX(df['h'], df['l'], df['c'],
+                           timeperiod=alpha.aDXInterval)
 
-    if df.at[-1, 'emaFast'] > df.at[-1, 'emaSlow'] and df.at[-2, 'emaFast'] < df.at[-2, 'emaSlow'] and df.at[-1, 'adx'] < 40 and df.at[-1, 'adx'] > 30:
+    if latestEMAFast > latestEMASlow and secLatestEMAFast < secLatestEMASlow and latestADX < 40 and latestADX > 30:
         alpha.signal = Signal.SHORT
         alpha.sigGenerated = True
-    elif df.at[-1, 'emaFast'] < df.at[-1, 'emaSlow'] and df.at[-2, 'emaFast'] > df.at[-2, 'emaSlow'] and df.at[-1, 'adx'] < 40 and df.at[-1, 'adx'] > 30:
+    elif latestEMAFast < latestEMASlow and secLatestEMAFast > secLatestEMASlow and latestADX < 40 and latestADX > 30:
         alpha.signal = Signal.LONG
         alpha.sigGenerated = True
+
+    secLatestEMAFast, secLatestEMASlow = latestEMAFast, latestEMASlow
 
     # reset df to 500 candles for memory management
     if len(df.index) > 5000:
@@ -109,7 +125,6 @@ async def on_tick(data: dict):
 @on_stream(topic="crypto.futures.ohlc.btcusdt")
 async def on_candle(data: dict):
     """Process data every candle"""
-    print(data)
     genSig(data)
 
 
