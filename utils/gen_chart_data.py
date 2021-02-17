9e15727ca0fb3b13b229939381916a89b777a937
MysterTech <sanjay.krishna@MysterTech.com>
2021-02-17 19:52:23 +0530

    [Update] Added metrics output to json

--- utils/gen_chart_data.py
+++ utils/gen_chart_data.py
@@ -12,6 +12,7 @@ from functools import reduce
 from sys import argv as rd
 import json
 from operator import add
+from analysis import generate_metrics
 
 from aiohttp import ClientSession
 import pandas as pd
@@ -22,12 +23,9 @@ BASE_URL = f"https://fapi.binance.com/fapi/v1/klines?symbol=BTCUSDT&interval=1m&
 
 def collect_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--input', type=str, default='orders.csv',
-                        help="Order data")
-    parser.add_argument('-c', '--candles', type=str, default='candles.csv',
-                        help="Candle data dump")
-    parser.add_argument('-o', '--output', type=str, default='data.json',
-                        help="Output charting json")
+    parser.add_argument("-i", "--input", type=str, default="orders.csv", help="Order data")
+    parser.add_argument("-c", "--candles", type=str, default="candles.csv", help="Candle data dump")
+    parser.add_argument("-o", "--output", type=str, default="data.json", help="Output charting json")
     args = parser.parse_args()
     return args
 
@@ -42,6 +40,7 @@ async def gen_candles(start_time: int, end_time: int) -> list:
     req_params = [(start * 1000, (start + N_LIM * 60) * 1000) for start in range(start_time, end_time, N_LIM * 60)]
     urls = map(lambda lims: BASE_URL + f"&startTime={lims[0]}&endTime={lims[-1]}", req_params)
 
+
 async def fetch_url(url: str, session: ClientSession) -> dict:
     async with session.get(url) as response:
         return await response.json()
@@ -51,56 +50,60 @@ async def gen_candles(start_time: int, end_time: int) -> list:
     """Generate list of ohlc candles"""
     req_params = [(start * 1000, (start + N_LIM * 60) * 1000) for start in range(start_time, end_time, N_LIM * 60)]
     urls = map(lambda lims: BASE_URL + f"&startTime={lims[0]}&endTime={lims[-1]}", req_params)
-    fields = ['time', 'open', 'high', 'low', 'close', 'volume']
+    fields = ["time", "open", "high", "low", "close", "volume"]
     async with ClientSession() as session:
-        chunks = map(lambda url: asyncio.ensure_future(fetch_url(url, session)),
-                     urls)
+        chunks = map(lambda url: asyncio.ensure_future(fetch_url(url, session)), urls)
         candles = reduce(add, await asyncio.gather(*chunks))
-        return map(lambda candle: dict(zip(fields, [int(candle[0] / 1000)] + candle[1:len(fields)])), candles)
+        return map(lambda candle: dict(zip(fields, [int(candle[0] / 1000)] + candle[1 : len(fields)])), candles)
 
 
 def parse_orders(filename) -> dict:
     df = pd.read_csv(filename, parse_dates=[0, 1])
-    df.entry_time = df.entry_time.round('T').map(lambda x: int(x.timestamp()))
-    df.exit_time = df.exit_time.round('T').map(lambda x: int(x.timestamp()))
+    df.entry_time = df.entry_time.round("T").map(lambda x: int(x.timestamp()))
+    df.exit_time = df.exit_time.round("T").map(lambda x: int(x.timestamp()))
+    metrics = generate_metrics(df)
     orders = []
     for _, trade in df.iterrows():
         entry = {
-            'time': trade.entry_time,
-            'position': 'belowBar' if trade.trade_type == "LONG" else 'aboveBar',
-            'color': 'green' if trade.status else 'red',
-            'shape': 'arrowUp' if trade.trade_type == "LONG" else 'arrowDown',
-            'id': f"id{_}-entry",
-            'text': f"Entry @ {trade.entry_price: 0.3f}",
-            'size': 0.8
+            "time": trade.entry_time,
+            "position": "belowBar" if trade.trade_type == "LONG" else "aboveBar",
+            "color": "green" if trade.status else "red",
+            "shape": "arrowUp" if trade.trade_type == "LONG" else "arrowDown",
+            "id": f"id{_}-entry",
+            "text": f"Entry @ {trade.entry_price: 0.3f}",
+            "size": 0.8,
         }
         orders.append(entry)
         exit = entry.copy()
-        exit.update({
-            'time': trade.exit_time,
-            'position': 'aboveBar' if trade.trade_type == "LONG" else 'belowBar',
-            'shape': 'arrowUp' if trade.trade_type == "SHORT" else 'arrowDown',
-            'id': f"id{_}-exit",
-            'text': f"Exit @ {trade.exit_price: 0.3f}",
-            'size': 0.5
-        })
+        exit.update(
+            {
+                "time": trade.exit_time,
+                "position": "aboveBar" if trade.trade_type == "LONG" else "belowBar",
+                "shape": "arrowUp" if trade.trade_type == "SHORT" else "arrowDown",
+                "id": f"id{_}-exit",
+                "text": f"Exit @ {trade.exit_price: 0.3f}",
+                "size": 0.5,
+            }
+        )
         orders.append(exit)
-    return orders, df.entry_time.min(), df.exit_time.max()
+    return orders, metrics, df.entry_time.min(), df.exit_time.max()
 
 
 async def main():
-   args = collect_args()
+    args = collect_args()
 
-   orders, start_time, end_time = parse_orders(args.input)
-   candles = await gen_candles(start_time, end_time)
+    orders, metrics_df, start_time, end_time = parse_orders(args.input)
+    candles = await gen_candles(start_time, end_time)
 
-   ohlc = pd.DataFrame.from_dict(list(candles)).set_index('time').sort_index()
-   ohlc.to_csv(args.candles)
+    ohlc = pd.DataFrame.from_dict(list(candles)).set_index("time").sort_index()
+    ohlc.to_csv(args.candles)
 
-   json.dump({
-        "candles": ohlc.reset_index().to_dict(orient='records'),
-        "orders": orders
-    }, open(args.output, 'w+'))
+    metrics = metrics_df.to_json()
+
+    json.dump(
+        {"candles": ohlc.reset_index().to_dict(orient="records"), "orders": orders, "metrics": metrics},
+        open(args.output, "w+"),
+    )
 
 
 if __name__ == "__main__":
